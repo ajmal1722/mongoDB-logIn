bea4e80e29a3ac2b5782d559f3bd277ca7100c10
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-12-26 18:57:22 +0530

    added some modules

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-const PORT = 3000;
+dotenv.config({path : 'config.env'})  // file path
+const PORT = process.env.PORT || 8080;
+
+// log requests
+app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyParser.urlencoded({extended:true}));
+
+// set view engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.send('crud app using mongoDB')
